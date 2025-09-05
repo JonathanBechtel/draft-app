99ed848663aab1a4e33019cc7d6ee36cc1c2c1ac
Jonathan Bechtel <jonathanbechtel@gmail.com>
2025-09-05 13:16:18 -0400

    add player model and update player retrieval endpoint to include position

--- app/main.py
+++ app/main.py
@@ -2,10 +2,35 @@
 Main entry point for FastAPI application.
 """
 from fastapi import FastAPI
+from enum import Enum
+from datetime import date
+from pydantic import BaseModel
+
+class Position(str, Enum):
+    f = "forward"
+    c = "center"
+    g = "guard"
+
+class Player(BaseModel):
+    name: str
+    position: Position
+    school: str
+    age: int
+    birth_year: date
 
 app = FastAPI()
 
+# notes
+# first arg in function automatically maps to path parameter, names must match
+
 @app.get("/players/{player_id}")
-async def find_player(player_id: int):
+async def find_player(player_id: int, position: Position = "guard"):
     return {"player_id": player_id,
+            "position": position,
             "name": "player name"}
+
+@app.post("/players/")
+async def create_player(player: Player):
+    # does just this encapsulate all the necessary logic?  
+    # anything I'm missing?
+    return player
