bda4f97d36eeb822784f6338bc01be623e74c4fd
Jonathan Bechtel <jonathanbechtel@gmail.com>
2025-09-07 15:28:15 -0400

    refactor: update player model and add player creation endpoint; enhance FastAPI app structure

--- app/main.py
+++ app/main.py
@@ -1,36 +1,40 @@
 """
 Main entry point for FastAPI application.
 """
+
 from fastapi import FastAPI
 from enum import Enum
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 class Position(str, Enum):
     f = "forward"
     c = "center"
     g = "guard"
 
+birth_year = Annotated[date, Field(..., ge=date(1980, 1, 1))]
+
 class Player(BaseModel):
     name: str
     position: Position
     school: str
     age: int
-    birth_year: date
+    birth_year: birth_year
 
 app = FastAPI()
 
-# notes
-# first arg in function automatically maps to path parameter, names must match
-
-@app.get("/players/{player_id}")
-async def find_player(player_id: int, position: Position = "guard"):
-    return {"player_id": player_id,
-            "position": position,
-            "name": "player name"}
+@app.delete("/players/{player_id}"):
+async def delete_player(player_id):
+    """Delete a player from the application"""
+    pass
 
 @app.post("/players/")
 async def create_player(player: Player):
-    # does just this encapsulate all the necessary logic?  
-    # anything I'm missing?
+    """Post request to store a players information"""
     return player
+
+@app.get("/players"):
+async def list_players():
+    """List all players from the database"""
+    pass
---
+++ app/models/players.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field, create_engine
+from typing import Literal, Annotated
+from datetime import date
+
+BIRTH_YEAR = Annotated[data, Field(..., ge=date(1980, 1, 1))]
+
+class Player(SQLModel, table=True):
+    # do you need to specify autoincrement?
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    position: str = Literal["forward", "center", "guard"]
+    school: str
+    age: int = Field(default=18, ge=18)
+    birth_year = BIRTH_YEAR
+
+
+
+
+
